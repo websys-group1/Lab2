bb2318471153f08ba5ffcd7c2e4ca28cdf14a1a5
RexY-RPI <yus8@rpi.edu>
2021-09-24 06:33:32 -0400

    Final Version of Lab2-Group1

---
+++ constitution.js
@@ -0,0 +1,263 @@
+
+$(document).ready(function(){ 
+  document.getElementById("preambletext").style.display='none';
+  });
+  function showButton(){
+    document.getElementById("preambletext").style.display='block';
+  }
+  $(document).ready(function(){
+    $("#preambleButton").click(function(){
+        $("#preambletext").toggle();
+    });
+});
+
+//-------------------------------------------------
+$(document).ready(function(){ 
+  document.getElementById("preambleRecap").style.display='none';
+  });
+  function showButton(){
+    document.getElementById("preambleRecap").style.display='block';
+  }
+
+  $(document).ready(function(){
+    $("#pRecapButton").click(function(){
+        $("#preambleRecap").toggle();
+    });
+});
+
+//-------------------------------------------------
+$(document).ready(function(){
+    document.getElementById("article1text").style.display='none';
+  
+  });
+  function showButton(){
+    document.getElementById("article1text").style.display='block';
+  }
+
+  $(document).ready(function(){
+    $("#button1").click(function(){
+        $("#article1text").toggle();
+    });
+});
+//-------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article2text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article2text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button2").click(function(){
+      $("#article2text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article3text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article3text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button3").click(function(){
+      $("#article3text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article4text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article4text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button4").click(function(){
+      $("#article4text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article5text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article5text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button5").click(function(){
+      $("#article5text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article6text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article6text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button6").click(function(){
+      $("#article6text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("article7text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("article7text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#button7").click(function(){
+      $("#article7text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen1text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen1text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton1").click(function(){
+      $("#amen1text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen2text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen2text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton2").click(function(){
+      $("#amen2text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen3text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen3text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton3").click(function(){
+      $("#amen3text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen4text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen4text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton4").click(function(){
+      $("#amen4text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen5text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen5text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton5").click(function(){
+      $("#amen5text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen6text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen6text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton6").click(function(){
+      $("#amen6text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen7text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen7text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton7").click(function(){
+      $("#amen7text").toggle();
+  });
+});
+//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen8text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen8text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton8").click(function(){
+      $("#amen8text").toggle();
+  });
+});//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen9text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen9text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton9").click(function(){
+      $("#amen9text").toggle();
+  });
+});//--------------------------------------------------
+$(document).ready(function(){
+  document.getElementById("amen10text").style.display='none';
+
+})
+function showButton(){
+  document.getElementById("amen10text").style.display='block';
+}
+
+$(document).ready(function(){
+  $("#amebutton10").click(function(){
+      $("#amen10text").toggle();
+  });
+});
